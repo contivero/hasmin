4527c321421c66bbe0d758081f3b0b30098ace25
Cristian Adrián Ontivero <cristianontivero@gmail.com>
2017-10-20 23:27:47 -0300

    Add 2 HLint warnings

--- HLint.hs
+++ HLint.hs
@@ -8,6 +8,8 @@ warn = pure x <* y ==> x <$ y
 warn = x >> return y ==> x $> y
 warn = return x << y ==> x <$ y
 warn = return x ==> pure x -- Every monad is an applicative functor
+warn = m >>= pure . f ==> f <$> m
+warn = pure . f =<< m ==> f <$> m
 
 -- Hasmin specific
 warn = skipComments *> x <* skipComments ==> lexeme x
